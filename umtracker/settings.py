d3a867a48458c167996313f2f04c8cd31f6ea8da
Daniil Leshchev <leshev.da@gmail.com>
2025-11-14 15:17:36 +0500

    cors_allowed_origins

--- umtracker/settings.py
+++ umtracker/settings.py
@@ -126,11 +126,11 @@ SIMPLE_JWT = {
 }
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8080",
-#     "http://127.0.0.1:8000",
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
